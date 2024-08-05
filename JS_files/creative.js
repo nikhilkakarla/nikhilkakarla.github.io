2df339872332c73c5b547f43ae185cc4a5f7a0a9
nikhilkakarla <nikhilkakarla@gmail.com>
2024-08-05 17:06:14 -0400

    more commit

--- JS_files/creative.js
+++ JS_files/creative.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const nextBtn = document.getElementById('next-btn');
 
     const videos = [
-        { src: 'montages/Second Semester Senior Year REDACTED.mp4', title: 'Second Semester Senior Year (01/01/2024 - 06/01/2024)' },
-        { src: 'montages/First Semester Senior Year 2 copy copy.mp4', title: 'First Semester Senior Year (09/01/2023 - 01/01/2024)' },
-        { src: 'montages/Summer2023Redacted.mp4', title: 'Summer of 2023 (05/25/2023 - 08/28/2024)' },
-        { src: 'montages/Grimmentz FInal copy copy.mp4', title: 'Skiing in Grimmentz (03/24/2023 - 04/03/2024)' },
-        { src: 'montages/Powder Chasers 2023.mp4', title: 'HeliSkiing in BC (01/03/2023 - 01/06/2023)' },
-        { src: 'montages/Skydiving.mp4', title: 'Skydiving (05/30/2024)' }
+        { src: 'https://www.youtube.com/embed/H6uiZ1IZhfA', title: 'Second Semester Senior Year (01/01/2024 - 06/01/2024)' },
+        { src: 'https://www.youtube.com/embed/Cq99woykFB4', title: 'First Semester Senior Year (09/01/2023 - 01/01/2024)' },
+        { src: 'https://www.youtube.com/embed/Y20P9mIFCrs', title: 'Summer of 2023 (05/25/2023 - 08/28/2024)' },
+        { src: 'https://www.youtube.com/embed/jMixDfDaB7A', title: 'Skiing in Grimmentz (03/24/2023 - 04/03/2024)' },
+        { src: 'https://www.youtube.com/embed/_qO3SoXMHTA', title: 'HeliSkiing in BC (01/03/2023 - 01/06/2023)' },
+        { src: 'https://www.youtube.com/embed/wY9j7mjD6Xo', title: 'Skydiving (05/30/2024)' }
     ];
 
     let currentVideoIndex = 0;
@@ -18,7 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
     function loadVideo(index) {
         videoPlayer.src = videos[index].src;
         videoTitle.innerText = videos[index].title;
-        videoPlayer.play();
         updateActiveThumbnail(index);
     }
 
